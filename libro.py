47a7fab8f0c22337c7e7e0913590cf47c9b0e8cc
facundolgomez <facundolgomez87@gmail.com>
2023-09-06 18:34:20 -0300

    correccion de la cantidad de libros prestados

--- libro.py
+++ libro.py
@@ -1,6 +1,6 @@
 # Crear un diccionario para cada libro
-libro1 = {'cod': 'CRBJsAkS', 'cant_ej_ad': 3, 'cant_ej_pr': 0, "titulo": "Cien años de soledad", "autor": "Gabriel García Márquez"}
-libro2 = {'cod': 'QgfV4j3c', 'cant_ej_ad': 4, 'cant_ej_pr': 0, "titulo": "El principito", "autor": "Antoine de Saint-Exupéry"}
+libro1 = {'cod': 'CRBJsAkS', 'cant_ej_ad': 3, 'cant_ej_pr': 1, "titulo": "Cien años de soledad", "autor": "Gabriel García Márquez"}
+libro2 = {'cod': 'QgfV4j3c', 'cant_ej_ad': 4, 'cant_ej_pr': 2, "titulo": "El principito", "autor": "Antoine de Saint-Exupéry"}
 libro3 = {'cod': 'adOd09UE', 'cant_ej_ad': 1, 'cant_ej_pr': 0, "titulo": "El código Da Vinci", "autor": "Dan Brown"}
 
 from cod_generator import generar
